7686d7f65bb3a34e1d9e7ca847ba9530a609b1db
Felipe Barbosa <felipebarbosadosreis092@gmail.com>
2023-08-01 10:59:11 -0300

    Initial commit

---
+++ ex_fixacao01.py
@@ -0,0 +1,4 @@
+print("Olá, esse é um programa que converte o valor em Dólar (US$) para Reais (R$)")
+print("")
+num = float(input("Qual o valor (em Dólar) a ser inserido?"))
+print("O valor convertido é de R$", num*4.78)
